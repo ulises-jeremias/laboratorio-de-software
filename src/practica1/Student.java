b019544da772f350191ed606ff1a05a37c734736
lucasdc <ldicunzolo@cespi.unlp.edu.ar>
2018-08-22 17:23:40 -0300

    Added initial projexct

---
+++ src/practica1/Student.java
@@ -0,0 +1,13 @@
+package practica1;
+
+public class Student {
+
+	private String lastname;
+	private String firstname;
+	private String number;
+	
+	public static void main(String[] args) {
+
+	}
+
+}
