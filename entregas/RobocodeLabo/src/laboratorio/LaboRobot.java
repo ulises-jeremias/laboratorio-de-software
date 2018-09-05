00f643d6b35017779aed7afe9acb0e2c9f2dd629
ucornejofandos <ucornejofandos@lifia.info.unlp.edu.ar>
2018-09-05 16:42:49 -0300

    Updated

---
+++ entregas/RobocodeLabo/src/laboratorio/LaboRobot.java
@@ -0,0 +1,48 @@
+package laboratorio;
+import robocode.*;
+
+// API help : http://robocode.sourceforge.net/docs/robocode/robocode/JuniorRobot.html
+
+
+public class LaboRobot extends JuniorRobot
+{
+
+	@Override	
+	public void run() {
+
+		setColors(orange, blue, white, yellow, black);
+
+
+		while(true) {
+		
+			ahead(100);
+			turnGunRight(360);
+			back(100);
+			turnGunRight(360);
+		}
+	}
+
+	/**
+	 * onScannedRobot: What to do when you see another robot
+	 */
+	@Override
+	public void onScannedRobot() {
+		fire(1);
+	}
+
+	/**
+	 * onHitByBullet: What to do when you're hit by a bullet
+	 */
+	@Override
+	public void onHitByBullet() {
+		back(10);
+	}
+	
+	/**
+	 * onHitWall: What to do when you hit a wall
+	 */
+	@Override
+	public void onHitWall() {
+		back(20);
+	}	
+}
