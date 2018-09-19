674ea082631e326f41f72f1ae3e730d1c39759bf
ulises-jeremias <ulisescf.24@gmail.com>
2018-09-19 01:25:12 -0300

    Se actualiza container principal y se agregan imagenes

--- entregas/tercer-entrega/project/src/composer/containers/PlayerContainer.java
+++ entregas/tercer-entrega/project/src/composer/containers/PlayerContainer.java
@@ -1,90 +1,131 @@
 package composer.containers;
 
+import java.awt.Component;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
 import javax.swing.ImageIcon;
-import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.UIManager;
-import org.eclipse.wb.swing.FocusTraversalOnArray;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
 
+import org.jfugue.player.Player;
+
+import composer.actions.MusicalNotes;
+import composer.components.Note;
+import composer.components.Notes;
 import composer.components.Pentagram;
 
-public class PlayerContainer {
-	private JFrame frame;
-	private JTextField textField;
+public class PlayerContainer extends JFrame {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 9213674316794588532L;
+	
+	private Player player;
+	private JTextField textField1;
+	private JTextField textField2;
+
+	protected Note selectedNote;
 
 	public PlayerContainer() {
-
+		super();
+		initialize();
 	}
 
-	public JFrame render() {
-		frame = new JFrame();
+	public void initialize() {
+		player = new Player();
 
-		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(null);
+		setBounds(100, 100, 450, 300);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		getContentPane().setLayout(null);
 
-		Pentagram pane = new Pentagram();
+		// Add pentagram to UI
+		getContentPane().add(new Pentagram());
 
-		frame.getContentPane().add(pane.render());
+		// Notes definition
+		JPanel notes = new Notes();
+		notes.setBorder(UIManager.getBorder("InternalFrame.paletteBorder"));
+		notes.setBounds(49, 108, 349, 58);
+		
+		// Buttons definition
+		JLabel play_1 = new JLabel("");
+		play_1.setIcon(new ImageIcon(getClass().getResource("../images/play.png")));
+		play_1.setBounds(275, 169, 32, 32);
+		getContentPane().add(play_1);
+		
+		JLabel play_2 = new JLabel("");
+		play_2.setIcon(new ImageIcon(getClass().getResource("../images/play.png")));
+		play_2.setBounds(316, 225, 32, 32);
+		getContentPane().add(play_2);
+		
+		JLabel clean = new JLabel("");
+		clean.setIcon(new ImageIcon(getClass().getResource("../images/clear.png")));
+		clean.setBounds(319, 169, 32, 32);
+		getContentPane().add(clean);
+		
+		// Inputs definition
+		JLabel undo = new JLabel("");
+		undo.setIcon(new ImageIcon(getClass().getResource("../images/undo.png")));
+		undo.setBounds(365, 178, 30, 30);
+		getContentPane().add(undo);
+		
+		textField1 = new JTextField();
+		textField1.setEnabled(false);
+		textField1.setBounds(49, 178, 215, 24);
+		getContentPane().add(textField1);
+		textField1.setColumns(10);
+		
+		textField2 = new JTextField();
+		textField2.setBounds(86, 233, 215, 24);
+		getContentPane().add(textField2);
+		textField2.setColumns(10);
+		
+		// Event handlers definitions
+		
+		textField2.getDocument().addDocumentListener(new DocumentListener() {
+			  public void changedUpdate(DocumentEvent e) {
+			    update();
+			  }
+			  public void removeUpdate(DocumentEvent e) {
+			    update();
+			  }
+			  public void insertUpdate(DocumentEvent e) {
+			    update();
+			  }
 
-		textField = new JTextField();
-		textField.setEnabled(false);
-		textField.setBounds(49, 178, 215, 24);
-		frame.getContentPane().add(textField);
-		textField.setColumns(10);
-
-		JPanel panel = new JPanel();
-		panel.setBorder(UIManager.getBorder("InternalFrame.paletteBorder"));
-		panel.setBounds(49, 108, 349, 58);
-		frame.getContentPane().add(panel);
-
-		JLabel lblNewLabel_2 = new JLabel("");
-		panel.add(lblNewLabel_2);
-		lblNewLabel_2.setIcon(new ImageIcon("../images/redonda.png"));
-
-		JLabel lblNewLabel_3 = new JLabel("");
-		panel.add(lblNewLabel_3);
-		lblNewLabel_3.setIcon(new ImageIcon("../images/blanca.png"));
-
-		JLabel lblNewLabel_6 = new JLabel("");
-		panel.add(lblNewLabel_6);
-		lblNewLabel_6.setIcon(new ImageIcon("../images/semicorchea.png"));
-
-		JLabel lblNewLabel_4 = new JLabel("");
-		panel.add(lblNewLabel_4);
-		lblNewLabel_4.setIcon(new ImageIcon("../images/negra.png"));
-
-		JLabel label_1 = new JLabel("");
-		panel.add(label_1);
-		label_1.setIcon(new ImageIcon("../images/fusa.png"));
-
-		JLabel label_2 = new JLabel("");
-		panel.add(label_2);
-		label_2.setIcon(new ImageIcon("../images/semifusa.png"));
-
-		JLabel lblNewLabel_5 = new JLabel("");
-		panel.add(lblNewLabel_5);
-		lblNewLabel_5.setIcon(new ImageIcon("../images/corchea.png"));
-		panel.setFocusTraversalPolicy(new FocusTraversalOnArray(new JComponent[]{lblNewLabel_2, lblNewLabel_3, lblNewLabel_6, lblNewLabel_4, label_1, label_2, lblNewLabel_5}));
-
-		JLabel label_3 = new JLabel("");
-		label_3.setIcon(new ImageIcon("../images/play.png"));
-		label_3.setBounds(275, 169, 32, 32);
-		frame.getContentPane().add(label_3);
-
-		JLabel label_4 = new JLabel("");
-		label_4.setIcon(new ImageIcon("../images/play.png"));
-		label_4.setBounds(316, 225, 32, 32);
-		frame.getContentPane().add(label_4);
-
-		JLabel label_5 = new JLabel("");
-		label_5.setIcon(new ImageIcon("../images/escoba.png"));
-		label_5.setBounds(319, 169, 32, 32);
-		frame.getContentPane().add(label_5);
-
-		return frame;
+			  public void update() {
+			     MusicalNotes.onTextFieldChange(textField2.getText());
+			  }
+			});
+		
+		for (Component note : notes.getComponents()) {
+		     note.addMouseListener(new MouseAdapter() {
+				
+			    public void mouseClicked(MouseEvent e) {
+			    	((Note) note).toggleSelected();
+			    	
+				    if (!((Note) note).isSelected()) {
+				    	selectedNote = null;
+				    	return;
+				    }
+				    
+				    if (selectedNote != null)
+			    		selectedNote.toggleSelected();
+			    	
+			    	selectedNote = (Note) note;
+			    }
+		    });
+		}
+		
+		play_2.addMouseListener(new MouseAdapter() {
+		    public void mouseClicked(MouseEvent e) {
+			    player.play(MusicalNotes.getNotesAsString());
+		    }
+	    });
 	}
 }
